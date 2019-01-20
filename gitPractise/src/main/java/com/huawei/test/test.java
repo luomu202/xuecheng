556379ba67aba755f87c728c1b71fc8533b18873
luomu <120943126@qq.com>
2019-01-20 15:20:27 +0800

    啊

---
+++ gitPractise/src/main/java/com/huawei/test/test.java
@@ -0,0 +1,13 @@
+package com.huawei.test;
+
+/**
+ * @author Luomu
+ * @create 2019-01-20 15:10
+ **/
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("haha");
+    }
+
+}
