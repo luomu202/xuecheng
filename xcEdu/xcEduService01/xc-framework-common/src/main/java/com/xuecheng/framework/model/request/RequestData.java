fbceaf61bc1aa1a12b5bfe8cc9ac13806624f537
luomu <120943126@qq.com>
2019-03-23 15:44:51 +0800

    dd1

---
+++ xcEdu/xcEduService01/xc-framework-common/src/main/java/com/xuecheng/framework/model/request/RequestData.java
@@ -0,0 +1,16 @@
+package com.xuecheng.framework.model.request;
+
+import lombok.Data;
+import lombok.ToString;
+
+/**
+ * Created by mrt on 2018/3/5.
+ */
+@Data
+@ToString
+public class RequestData {
+
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
---
+++ xcEdu/xcEduService01/xc-framework-common/src/main/java/com/xuecheng/framework/model/request/sss.java
@@ -0,0 +1,9 @@
+package com.xuecheng.framework.model.request;
+
+/**
+ * @author Luomu
+ * @create 2019-02-10 14:21
+ **/
+public class sss {
+
+}
